83cc38b41e0db8dc32005c2637f9346c40f3b5d2
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-01-12 14:53:58 +0530

    "CSS file Name issue"

--- src/Components/CountryDetail.jsx
+++ src/Components/CountryDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./CountryDetail.css";
+import "./countryDetail.css";
 import { Link, useLocation, useParams } from "react-router-dom";
 import CountryDetailShimmer from "./CountryDetailShimmer";
 
